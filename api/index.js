459e55b946226d4bffff82eb9a1a6e78b52477f9
Jhon Alexander Garcia Sierra <jhon@booklick.co>
2024-07-26 21:20:46 -0500

    Gestión de errores

--- api/index.js
+++ api/index.js
@@ -8,6 +8,8 @@ const config = require('../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
 
+const errors = require('../network/errors')
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -18,6 +20,10 @@ const swaggerDoc = require('./swagger.json')
 app.use('/api/user', user)
 app.use('/api/auth', auth)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
+
+// Se debe colocar de ultimas
+app.use(errors)
 
 app.listen(config.api.port, () => {
     console.log('Api escuchando en el puerto', config.api.port);
